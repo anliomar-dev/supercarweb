b9bbe4c35e48f30381bc1540ce759f9cb6f5e3ff
Anli omar <omaranli56@gmail.com>
2024-02-29 17:03:39 +0400

    added imgaes in admin/imges

---
+++ admin/script_php/login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!--lien font awsome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
+        <link rel="stylesheet" href="../style/style.css">
+    </head>
+    <body>
+        <div class="container-fluid d-flex justify-content-center align-items-center text-center">
+            <div class="row">
+                <div class="col p-3">
+                    <div class="row">
+                        <div class="col-12"><h5>Espace administrateur</h5></div>
+                        <div class="col-12"><img src="../images/logo_adminb.png" alt="" class="img-fluid"></div>
+                        <form action="" class="col-12">
+                            <input type="text" class="text-center mt-3" placeholder="Identifiant">
+                            <input type="password" name="" id="" class="text-center mt-3" placeholder="Mot de passe">
+                            <input type="submit" class="mt-3">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>  
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
