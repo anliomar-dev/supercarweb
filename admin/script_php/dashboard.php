79b316e55cf0e599cab260cf65d5a4b161235c8b
Anli omar <omaranli56@gmail.com>
2024-03-05 16:57:16 +0400

    added two files: dashboard.php and dashboard.css

---
+++ admin/script_php/dashboard.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!--lien font awsome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="../style/dashboard.css">
+    </head>
+    <body>
+        <div class="container-fluid entete">
+            <div class="row">
+                <div class="col-6"><p>Supercar administration</p></div>
+                <div class="col-3">Bienvenue user</div>
+            </div>
+        </div>
+        <div class="container-fluid mt-3">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4 border pt-5 px-5 left">
+                    <div class="row">
+                        <div class="col-12 p-3">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12 titre-h4">
+                                            <h4>Authetification et authorisation</h4>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="users">Utilisateurs</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 mt-5">
+                                    <div class="row">
+                                        <div class="col-12 titre-h4">
+                                            <h4>Tables</h4>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="table">voitures</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="table">Modèles</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="table">Marques</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="table">Inscriptions</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                        <div class="col-12 pt-3 border d-flex justify-content-between px-3">
+                                            <p class="table">Contacts</p>
+                                            <a href="">➕add</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-8 border text-center pt-5 px-5 right">
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-5 col-lg-5 m-3">1</div>
+                        <div class="col-12 col-sm-12 col-md-5 col-lg-5 m-3">2</div>
+                        <div class="col-12 col-sm-12 col-md-5 col-lg-5 m-3">3</div>
+                        <div class="col-12 col-sm-12 col-md-5 col-lg-5 m-3">4</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+    </body>
+</html>
